750af17b84f19c3f3b84034aac16153accb10499
DevTnbad <dev.tnbad@gmail.com>
2023-10-26 18:02:34 -0300

    primeiro comit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.json('Ok')
+});
+
+const porta = process.env.PORTA || 3000;
+app.listen(process.env.PORT, () => console.log(`Servidor rodando na porta:${porta}`));
