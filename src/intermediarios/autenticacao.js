a0c05cf9d6ef341dbcc96ebcfa7601de006a60ee
DevTnbad <dev.tnbad@gmail.com>
2023-10-29 14:53:49 -0300

    ok

--- src/intermediarios/autenticacao.js
+++ src/intermediarios/autenticacao.js
@@ -13,6 +13,13 @@ const autenticacao = async (req, res, next) => {
         const token = authorization.split(' ')[1];
         const verificado = jwt.verify(token, chaveSecreta);
 
+        const usuarioExiste = await knex('usuarios')
+            .where({ id: verificado.id, email: verificado.email }).first();
+
+        if (!usuarioExiste) { // colocar msg token inválido ou usuario nao encontrado ?
+            return res.status(400).json({ mensagem: 'Usuario nao encontrado.' })
+        }
+
         req.usuarioLogado = {
             id: verificado.id,
             nome: verificado.nome,
