9faf855be03ee0956ce934950d594405b600b943
DevTnbad <dev.tnbad@gmail.com>
2023-11-01 20:03:05 -0300

    atualizacao do pdv.sql com as tabelas produtos e clientes

--- pdv.sql
+++ pdv.sql
@@ -25,3 +25,24 @@ create table usuarios (
    ('Bebê'),
    ('Games')
    ;
+
+create table produtos (
+    id primary key serial,
+    descricao text,
+    quantidade_estoque int not null,
+    valor int not null,
+    categoria_id int references categorias(id)
+   );
+
+   create table clientes(
+    id primary key serial,
+    nome text,
+    email text unique not null,
+    cpf text unique not null,
+    cep text,
+    rua text,
+    numero text,
+    bairro text,
+    cidade text,
+    estado text
+   );
