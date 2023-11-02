3495da53aca51ff683b6356c1a74770f30ea9525
Lothar <lothar223@hotmail.com>
2023-11-02 09:35:31 -0300

    Adicionado cadastro de produto

--- src/controladores/produto/cadastrar.js
+++ src/controladores/produto/cadastrar.js
@@ -1,6 +1,23 @@
+const knex = require('../../conexoes/bancoDeDados');
 
-const cadastrar = async (req, res) => {
+const cadastrarProduto = async (req, res) => {
+    const { descricao, quantidade_estoque, valor, categoria_id } = req.body
+
+    try {
+        
+        const cadastroDeProduto = await knex('produtos').insert({
+            descricao,
+            quantidade_estoque,
+            valor,
+            categoria_id
+        }).returning('*')
+
+        return res.status(201).json(cadastroDeProduto[0])
+    } catch (error) {
+        console.log({ 'mensagem': erro.message });
+        return res.status(500).json({ "mensagem": "Erro interno do Servidor." });
+    }
 
 }
 
-module.exports = cadastrar;
+module.exports = cadastrarProduto;
