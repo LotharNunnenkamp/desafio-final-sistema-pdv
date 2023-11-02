4244d6ffcfc66f4e01356266e426c9b58f4138ad
DevTnbad <dev.tnbad@gmail.com>
2023-11-02 08:12:08 -0300

    endpoint  de produto/detalharProdutoPorId.js finalizado

--- src/controladores/produto/detalharProdutoPorId.js
+++ src/controladores/produto/detalharProdutoPorId.js
@@ -1,6 +1,19 @@
+const knex = require("../../conexoes/bancoDeDados");
 
 const detalharProdutoPorId = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const produtoExiste = await knex('produtos').where({ id }).first();
+        if (!produtoExiste) {
+            return res.status(404).json({ mensagem: 'Não foi encontrado produto para o id informado.' });
+        }
 
+        return res.status(200).json(produtoExiste);
+
+    } catch (erro) {
+        console.log(erro.message);
+        return res.status('Erro interno do servidor.');
+    }
 }
 
 module.exports = detalharProdutoPorId;
