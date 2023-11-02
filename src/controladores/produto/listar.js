88133d2de582d14d150a00a9d04f760537104e99
DevTnbad <dev.tnbad@gmail.com>
2023-11-01 22:38:36 -0300

    endpoint de listar com filtragem por categoria_id finalizado

--- src/controladores/produto/listar.js
+++ src/controladores/produto/listar.js
@@ -1,9 +1,19 @@
 const knex = require('../../conexoes/bancoDeDados');
 
 const listar = async (req, res) => {
+    const { categoria_id } = req.query;
     try {
+        if (categoria_id) {
+            const categoriaExiste = await knex('categorias').where({ id: categoria_id }).first();
+
+            if (categoriaExiste) {
+                const listarProdutosFiltrados = await knex('produtos').where({ categoria_id });
+                return res.status(200).json(listarProdutosFiltrados);
+            }
+        }
 
         const listarProdutos = await knex('produtos');
+
         return res.status(200).json(listarProdutos);
     } catch (erro) {
         console.log(erro.message);
