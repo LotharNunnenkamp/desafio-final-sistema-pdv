6cd94edb2fdad4c0d10e73da020c4e3926d0f21c
Pablo Diniz Franca <pablo.diniz0912@gmail.com>
2023-11-01 22:45:50 -0300

    adicionando o endpoint de listar clientes

--- src/rotas/cliente.js
+++ src/rotas/cliente.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const cadastrar = require('../controladores/cliente/cadastrar');
 const autenticacao = require('../intermediarios/autenticacao');
+const listar = require('../controladores/cliente/listar');
 
 const rotasCliente = Router();
 rotasCliente.use(autenticacao);
 
 // rotasCliente.post('/Cliente', cadastrar);
 // rotasCliente.post('/Cliente', editar);
-// rotasCliente.post('/Cliente', listar);
+rotasCliente.get('/cliente', listar);
 // rotasCliente.post('/Cliente', detalhar);
 
 module.exports = rotasCliente;
