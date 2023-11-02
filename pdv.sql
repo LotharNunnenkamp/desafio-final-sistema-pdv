9f21535e9d27aa3e2fe50ac68e1dc085405a02f9
unknown <jr.goes_@hotmail.com>
2023-11-02 11:36:27 -0300

    correção de mensagem e atualização de banco de dados

--- pdv.sql
+++ pdv.sql
@@ -1,27 +1,42 @@
 -- create database pdv;
 
-create table usuarios (
-  id serial primary key not null,
-  nome text not null,
-  email text unique not null,
-  senha text not null
-  );
-  
- create table categorias (
-   id serial primary key not null,
-   descricao text not null
-   );
-   
-   insert into categorias
-   (descricao)
-   values
-   ('Informática'),
-   ('Celulares'),
-   ('Beleza e Perfumaria'),
-   ('Mercado'),
-   ('Livros e Papelaria'),
-   ('Brinquedos'),
-   ('Moda'),
-   ('Bebê'),
-   ('Games')
-   ;
+create table
+    usuarios (
+        id serial primary key not null,
+        nome text not null,
+        email text unique not null,
+        senha text not null
+    );
+
+create table
+    categorias (
+        id serial primary key not null,
+        descricao text not null
+    );
+
+insert into
+    categorias (descricao)
+values ('Informática'), ('Celulares'), ('Beleza e Perfumaria'), ('Mercado'), ('Livros e Papelaria'), ('Brinquedos'), ('Moda'), ('Bebê'), ('Games');
+
+create table
+    produtos (
+        id primary key serial,
+        descricao text,
+        quantidade_estoque int not null,
+        valor int not null,
+        categoria_id int references categorias(id)
+    );
+
+create table
+    clientes(
+        id primary key serial,
+        nome text,
+        email text unique not null,
+        cpf text unique not null,
+        cep text,
+        rua text,
+        numero text,
+        bairro text,
+        cidade text,
+        estado text
+    );
