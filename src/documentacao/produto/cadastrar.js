47615a46ad11c752cadd0a853eec5688ccc2c0fe
Lothar <lothar223@hotmail.com>
2023-11-06 08:27:56 -0300

    Adicionadas configurações Swagger para endpoints Cadastrar e Editar produto

---
+++ src/documentacao/produto/cadastrar.js
@@ -0,0 +1,54 @@
+/**
+ * @swagger
+ * /usuario:
+ *   post:
+ *     tags:
+ *       - Produtos
+ *     summary: Cadastrar Produtos
+ *     description: Cria um novo produto.
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               descricao:
+ *                 type: string
+ *                 description: Descrição do produto.
+ *               quantidade_estoque:
+ *                 type: integer
+ *                 description: Quantidade em estoque do produto.
+ *               valor:
+ *                 type: float
+ *                 description: Valor do produto.
+ *               categoria_id:
+ *                 type: integer
+ *                 description: ID da categoria do produto.
+ *     responses:
+ *       201:
+ *         description: Produto cadastrado com sucesso.
+ *         content:
+ *           application/json:
+ *             example:
+ *               descricao: nome do produto
+ *               quantidade_estoque: 2
+ *               valor: 1500
+ *               categoria_id: 3 
+ *       404:
+ *         description: Categoria informada não existe.
+ *       401:
+ *         description: Não autorizado. O token JWT é necessário.
+ *       500:
+ *         description: Erro interno do servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 mensagem:
+ *                   type: string
+ *                   example: Erro interno do servidor.
+ */
---
+++ src/documentacao/produto/editar.js
@@ -0,0 +1,47 @@
+/**
+ * @swagger
+ * /usuario:
+ *   put:
+ *     tags:
+ *       - Produtos
+ *     summary: Editar informações do produto
+ *     description: Edita as informações de um produto cadastrado.
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               descricao:
+ *                 type: string
+ *                 description: Descrição do produto.
+ *               quantidade_estoque:
+ *                 type: integer
+ *                 description: Quantidade em estoque do produto.
+ *               valor:
+ *                 type: float
+ *                 description: Valor do produto.
+ *               categoria_id:
+ *                 type: integer
+ *                 description: ID da categoria do produto.
+ *     responses:
+ *       204:
+ *         description: Sucesso. As informações do produto foram atualizadas.
+ *       404:
+ *         description: Produto informado ou categoria informada não existe.
+ *       401:
+ *         description: Não autorizado. O token JWT é necessário.
+ *       500:
+ *         description: Erro interno do servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 mensagem:
+ *                   type: string
+ *                   example: Erro interno do servidor.
+ */
