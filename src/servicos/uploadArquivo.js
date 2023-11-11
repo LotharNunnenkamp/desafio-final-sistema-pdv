ad681284c57e176d71429f37c1ec85085572c322
DevTnbad <dev.tnbad@gmail.com>
2023-11-11 10:40:12 -0300

    aprimoramentos excluir de produtos

--- src/servicos/uploadArquivo.js
+++ src/servicos/uploadArquivo.js
@@ -2,7 +2,7 @@ const blackBlazeS3 = require('../conexoes/armazenamento');
 
 const uploadImagem = async (path, buffer, mimetype) => {
     const imagem = await blackBlazeS3.upload({
-        Bucket: process.env.BACKBLAZE_BUCKET,
+        Bucket: process.env.BLACKBLAZE_BUCKET,
         Key: path,
         Body: buffer,
         ContentType: mimetype
@@ -10,7 +10,7 @@ const uploadImagem = async (path, buffer, mimetype) => {
 
     return {
         path: imagem.Key,
-        url: `https://${process.env.BACKBLAZE_BUCKET}.${process.env.ENDPOINT_S3}/${imagem.Key}`
+        url: `https://${process.env.BLACKBLAZE_BUCKET}.${process.env.ENDPOINT_S3}/${imagem.Key}`
     }
 }
 
