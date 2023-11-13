04999a3b253dcb25b811a072a65d0d3274c72961
unknown <jr.goes_@hotmail.com>
2023-11-13 09:56:15 -0300

    refatorado controlador de cadastrar

--- src/controladores/pedido/cadastrar.js
+++ src/controladores/pedido/cadastrar.js
@@ -1,40 +1,60 @@
+const knex = require("knex");
 
 const cadastrar = async (req, res) => {
 
     const { cliente_id, observacao, pedido_produtos } = req.body;
 
-    // Verifica se todos os campos obrigatórios estão presentes na requisição
-    if (!cliente_id || !observacao || !pedido_produtos || pedido_produtos.length === 0) {
-        return res.status(400).json({ message: 'Campos obrigatórios faltando ou inválidos' });
-    }
-
     try {
         let valorTotal = 0;
+        let erros = [];
         // Verificar se os produtos existem e descobrir o valor total do pedido
         for (const produto of pedido_produtos) {
-            const ProdutoEncontrado = await knex('produtos').select('id').where('id', produto.produto_id);
-            if (ProdutoEncontrado.length < 1) {
-                return res.status(404).json({ message: `Produto com ID ${produto.produto_id} não encontrado` });
+            let produtoEncontrado = await knex('produtos').select('id').where('id', produto.produto_id).first();
+            if (!produtoEncontrado) {
+                erros.push({ message: `Produto com ID ${produto.produto_id} não encontrado` });
+                continue
             }
-            valorTotal += ProdutoEncontrado[0].valor * produto.quantidade_produto;
+
+            if (produto.quantidade_produto > produtoEncontrado.quantidade_estoque) {
+                erros.push({
+                    mensagem: `A quantidade solicitada: ${produto.quantidade_produto} para o produto de ID: ${produtoEncontrado.id} é maior que a quantidade atual em estoque: ${produtoEncontrado.quantidade_estoque}`
+                })
+                continue
+            }
+
+            valorTotal += produtoEncontrado.valor * produto.quantidade_produto;
+
+            produto.valor_produto = produtoEncontrado.valor;
+            produto.quantidade_estoque = produtoEncontrado;
+        }
+
+        if (erros.length > 0) {
+            console.log({ erros })
+            return res.status(400).json({ erros })
         }
 
         // Inserir o pedido
-        const cadastrarPedido = await knex('pedidos').insert({ cliente_id, observacao, valor_total: valorTotal }).returning('id');
+        const cadastrarPedido = await knex('pedidos').insert({ cliente_id, observacao, valor_total: valorTotal }).returning('*');
         const pedidoId = cadastrarPedido[0].id
         // Associar os produtos ao pedido
         for (const produto of pedido_produtos) {
-            const valorProduto = knex('produtos').select('valor').where('id', produto.produto_id)
+            // const valorProduto = knex('produtos').select('valor').where('id', produto.produto_id)
 
             const ProdutoPedidoObj = {
                 pedido_id: pedidoId,
                 produto_id: produto.produto_id,
                 quantidade_produto: produto.quantidade_produto,
-                valor_produto: valorProduto
+                valor_produto: produto.valo_produto
             }
 
-            const cadastrarProdutosPedidos = await knex('pedido_produtos').insert(ProdutoPedidoObj)
+            await knex('pedido_produtos').insert(ProdutoPedidoObj)
+
+            let descontarEstoque = produto.quantidade_estoque - produto.quantidade_produto
+
+            await knex('produtos').where('id', produto.produto_id)
+                .update({ quantidade_estoque: descontarEstoque })
         }
+
 
         return res.status(201).json({ message: 'Pedido cadastrado com sucesso' });
     } catch (error) {
