57f2c73624474f1cfdc9a3779b123898aad922d7
unknown <jr.goes_@hotmail.com>
2023-11-13 10:02:05 -0300

    atualizada rotas de cadastrar pedido

--- src/rotas/pedido.js
+++ src/rotas/pedido.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const cadastrar = require('../controladores/pedido/cadastrar');
 const listar = require('../controladores/pedido/listar');
+const validarCorpoRequisicao = require('../intermediarios/validarCorpoRequisicao');
+const schemaPedido = require('../validacoes/schemaPedido');
 
 
 const rotasPedido = Router();
 
-rotasPedido.post('/pedido', cadastrar);
+rotasPedido.post('/pedido', validarCorpoRequisicao(schemaPedido), cadastrar);
 rotasPedido.get('/pedido', listar);
 
 module.exports = rotasPedido;
