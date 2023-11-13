ea1e149cebb2a6cf2271344d73661dd63f9b1dc5
unknown <jr.goes_@hotmail.com>
2023-11-13 09:23:11 -0300

    refatorado o schemaPedido

--- src/validacoes/schemaPedido.js
+++ src/validacoes/schemaPedido.js
@@ -3,13 +3,14 @@ const { pt } = require('yup-locales');
 yup.setLocale(pt);
 const schemaPedido = yup.object({
     cliente_id: yup.number().required(),
-    observacao: yup.string(),
-    pedido_produtos: yup.required().array().of(
-        yup.object().shape({
-            produto_id: yup.number().required(),
-            quantidade_produto: yup.number().integer().moreThan(0).required()
+    observacao: yup.string().default(''),
+    pedido_produtos: yup.array().of(
+        yup.object({
+            produto_id: yup.number().positive().integer().required(),
+            quantidade_produto: yup.number().positive().integer().moreThan(0).required()
         })
     )
+        .min(1).required()
 });
 
 module.exports = schemaPedido;
