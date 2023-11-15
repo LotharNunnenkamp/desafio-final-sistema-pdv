13de71fb8d8269980ddd94e5d989b4c061963a8d
DevTnbad <dev.tnbad@gmail.com>
2023-11-15 08:43:20 -0300

    ajuste saida de listagem sem fintro

--- src/controladores/pedido/listar.js
+++ src/controladores/pedido/listar.js
@@ -1,10 +1,11 @@
 const knex = require('../../conexoes/bancoDeDados');
+const saidaFormatada = require('../../funcoesUteis/combinarPedido');
 
 const listar = async (req, res) => {
     const { cliente_id } = req.query;
     try {
         if (cliente_id) {
-            const clienteExiste = await knex('clientes').where({id: cliente_id}).first();
+            const clienteExiste = await knex('clientes').where({ id: cliente_id }).first();
             if (!clienteExiste) {
                 return res.status(404).json({ mensagem: 'Cliente não encontrado.' });
             }
@@ -14,20 +15,24 @@ const listar = async (req, res) => {
                 return res.status(404).json({ mensagem: 'Não há pedidos para este cliente.' });
             }
 
-            const produtosPedido = await knex('pedido_produtos').where({pedido_id: pedido.id});
+            const produtosPedido = await knex('pedido_produtos').where({ pedido_id: pedido.id });
 
-            return res.status(200).json({ 
+            return res.status(200).json({
                 pedido,
                 pedido_produtos: produtosPedido
             });
         }
 
         const pedidos = await knex('pedidos')
+        const produtosPedido = await knex('pedido_produtos')
+
+        const saida = saidaFormatada(pedidos, produtosPedido);
+
         if (pedidos.length === 0) {
             return res.status(404).json({ mensagem: 'Nenhum pedido encontrado.' });
         }
 
-        return res.status(200).json({pedidos});
+        return res.status(200).json(saida);
 
     } catch (error) {
         console.log({ 'mensagem': error.message });
