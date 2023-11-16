6a9c8e7fc0f063783e6e0e79bcc2c58e385f5abf
DevTnbad <dev.tnbad@gmail.com>
2023-11-16 12:42:33 -0300

    ajuste documentacao cadastrar e atualizar produto

--- src/documentacao/produto/editar.js
+++ src/documentacao/produto/editar.js
@@ -17,31 +17,31 @@
  *     requestBody:
  *       required: true
  *       content:
- *         application/json:
+ *         multipart/form-data:
  *           schema:
  *             type: object
  *             properties:
  *               descricao:
  *                 type: string
- *                 example: Notebook XPTO
  *                 description: Descrição do produto.
  *               quantidade_estoque:
  *                 type: integer
- *                 example: 2
  *                 description: Quantidade em estoque do produto.
  *               valor:
- *                 type: float
- *                 example: 324999
+ *                 type: number
  *                 description: Valor do produto.
  *               categoria_id:
  *                 type: integer
- *                 example: 1
  *                 description: ID da categoria do produto.
+ *               produto_imagem:
+ *                 type: string
+ *                 format: binary
+ *                 description: Imagem do produto (opcion
  *     responses:
  *       204:
  *         description: Sucesso. As informações do produto foram atualizadas.
  *       404:
- *         description: Produto informado ou categoria informada não existe.
+ *         description: Produto informado não existe.
  *         content:
  *           application/json:
  *             schema:
@@ -49,7 +49,7 @@
  *               properties:
  *                 mensagem:
  *                   type: string
- *                   example: O produto ou a categoria informada não existe. 
+ *                   example: O produto informado não existe. 
  *       401:
  *         description: Não autorizado. O token JWT é necessário.
  *         content:
@@ -59,7 +59,7 @@
  *               properties:
  *                 mensagem:
  *                   type: string
- *                   example: Usuario não autorizado. 
+ *                   example: Usuário não autorizado. 
  *       500:
  *         description: Erro interno do servidor.
  *         content:
