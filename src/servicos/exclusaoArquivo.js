966e44f6e5b464dd61f8bf64eab94d02ed493040
DevTnbad <dev.tnbad@gmail.com>
2023-11-17 09:29:45 -0300

    retirados comentarios e logs

--- src/servicos/exclusaoArquivo.js
+++ src/servicos/exclusaoArquivo.js
@@ -1,7 +1,6 @@
 const blackBlazeS3 = require('../conexoes/armazenamento');
 
 const excluirImagem = async (path) => {
-    console.log(path);
     await blackBlazeS3.deleteObject({
         Bucket: process.env.BLACKBLAZE_BUCKET,
         Key: path
